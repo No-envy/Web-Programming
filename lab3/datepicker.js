8abcef1de55f29d5a8540c21ac34cbb1be657244
No-envy <71056151+No-envy@users.noreply.github.com>
2020-12-23 14:35:06 +0800

    Update datepicker.js

--- lab3/datepicker.js
+++ lab3/datepicker.js
@@ -7,28 +7,21 @@ class DatePicker {
     }
 
     render(date) {
-        // create calender and add it to id div.
         var parent = document.getElementById(this.id);
         parent.appendChild(this._createCalender(date));
     }
 
     _createCalender(date) {
-        // 1. create a table add it to the document.
         var table = document.createElement("table");
-        // table.style.border = "dotted black";
-        
-        // 2. create table's header. 
+
         var header = this._createCalenderHeader(table, date);
  
-        // 3. add days of week on the second row
         var daysOfWeek = ["Sun", "Mon","Tues","Wed","Thu","Fri","Sat"];
         var rowWeek = header.insertRow(1);
         for (var i = 0; i < 7; ++ i) {
             var cell = rowWeek.insertCell(i);
             cell.innerHTML = daysOfWeek[i];
         }
-        
-        // 4. set normal days.
         var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
         var curDate = new Date(firstDay.getTime());
         curDate.setDate(-firstDay.getDay() + 1);
@@ -37,7 +30,6 @@ class DatePicker {
         while (true) {
             var row = table.insertRow(rowIndex);
             rowIndex = rowIndex + 1;
-            
             for (var i = 0; i < 7; ++ i) {
                 var cell = row.insertCell(i);
                 cell.innerHTML = curDate.getDate();
@@ -55,19 +47,14 @@ class DatePicker {
                 } else {
                     cell.setAttribute("id", "OtherMonth");
                 }
-
                 curDate.setDate(curDate.getDate() + 1);
             }
-
-            // exit the loop
             if (curDate.getMonth() != date.getMonth()) {
                 break;
             }
         }
         return table;
     }
-
-    /*  create header: "<  January   >" */
     _createCalenderHeader(table, date) {
         var header = table.createTHead();
         var headerRow = header.insertRow(0);
@@ -85,15 +72,12 @@ class DatePicker {
         var rightArrowCell = headerRow.insertCell(2);
         rightArrowCell.innerHTML = ">";
         rightArrowCell.setAttribute("id", "RightArrow");
-
-        // event handler.
         leftArrowCell.addEventListener("click", () => {
             table.remove();
             date.setMonth(date.getMonth() - 1);
             console.log(date);
             this.render(date);
         });
-
         rightArrowCell.addEventListener("click", () => {
             table.remove();
             date.setMonth(date.getMonth() + 1);
